761dac027be507aad93516dad0052f99f0008619
Etienne Detoisien <detoisienetienne@gmail.com>
2024-03-21 21:46:42 +0100

    detail page

---
+++ pages/contact.py
@@ -0,0 +1,2 @@
+def page_contact():
+    return "page contact"
--- pages/detail/[keyword]/[id].py
+++ pages/detail/[keyword]/[id].py
@@ -1,33 +1,48 @@
 from flask import render_template, redirect, url_for, request
 from utils import query_db
+from datetime import datetime
+
 
 def page_detail(keyword, id):
+    context = {}
     if request.method == "POST":
         return redirect(url_for("page_home"))
     if keyword == "acteur":
-        query = """select L.nomCompletL 
+        query = """select L.urlL 
         from langue L, acteur A, parler P
         where A.idA = P.idA
         and P.nomL = L.nomL
-        and idA = ?;
+        and A.idA = ?;
         """
         params = [id]
         languages = query_db(query, params)[0]
         query = "select * from acteur where idA = ?"
-    if keyword == "FilmSerie":
+    elif keyword == "FilmSerie":
         query = "select * from FilmSerie where refFS = ?"
     else:
-        return redirect("page_home")
+        return redirect(url_for("page_home"))
     params = [id]
     item = query_db(query, params)[0][0]
+    print(item)
+    context["item"] = item
+    
+    
+    
     if keyword == "acteur":
         query = """select FS.refFS, FS.nomFS, FS.poster_path, 'FilmSerie' as keyword 
         from FilmSerie FS, acteur A, jouer J
-        where idA = ?
+        where A.idA = ?
         and A.idA = J.idA
         and J.refFS = FS.refFS;"""
-        
+
     if keyword == "FilmSerie":
+        query = """select L.urlL 
+        from langue L, FilmSerie FS
+        where FS.refFS = ?
+        and FS.nomL = L.nomL
+        """
+        params = [id]
+        languages = query_db(query, params)[0]
         query = """select A.idA, A.nomA, A.profile_path, 'acteur' as keyword 
         from acteur A, jouer J, FilmSerie FS 
         where FS.refFS = ? 
@@ -35,6 +50,28 @@ def page_detail(keyword, id):
         and FS.refFS = J.refFS;"""
     params = [id]
     data = query_db(query, params)[0]
-    return render_template("detail.html", item=item, data=data, keyword=keyword, languages=languages)
+    
+    context["data"] = data
+    context["keyword"] = keyword
+    context["languages"] = languages
+    
+    if keyword == "FilmSerie":
+        query = """select G.nomG
+        from genre G, FilmSerie FS, appartenir A
+        where FS.refFS = ?
+        and FS.refFS = A.refFS
+        and A.idG = G.idG;
+        """
+        params = [id]
+        genres = query_db(query, params)[0]
+        context["genres"] = genres
+        percentage = context["item"][5] * 10
+        context["percentage"] = percentage
+        date = context["item"][6]
+        date = datetime.strptime(date, "%Y-%m-%d")
+        date = date.strftime("%d/%m/%Y")
+        context["date"] = date
+    return render_template("detail.html", **context)
+
 
 page_methods = ['GET', 'POST']
